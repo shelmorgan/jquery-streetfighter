4c904913c2d72e4b287a15d0f6fc9330d03203c2
Shel Morgan <shelbymorganbell@gmail.com>
2016-05-31 18:04:06 -0700

    added animation and audio

--- js/app.js
+++ js/app.js
@@ -9,11 +9,17 @@ $(document).ready(function() {
   })
   .mousedown(function() {
     console.log('mousedown');
-    //play hadouken sound
+    playHadouken();
     $('.ryu-ready').hide();
     $('.ryu-throwing').show();
-    $('.hadouken').show();
-    // animate hadouken to the right of the screen
+    $('.hadouken').finish().show().animate(
+      {'left': "1020px"},
+      500,
+      function() {
+        $(this).hide();
+        $(this).css('left', '520px');
+      }
+    );
   })
   .mouseup(function() {
     console.log('mouseup')
@@ -21,3 +27,9 @@ $(document).ready(function() {
     $('.ryu-ready').show();
   });
 });
+
+function playHadouken () {
+  $('#hadouken-sound')[0].volume = 0.5;
+  $('#hadouken-sound')[0].load();
+  $('#hadouken-sound')[0].play();
+}
