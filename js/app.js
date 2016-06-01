42ab60bc398e6ffd1388fd87bc3b86351343d0e0
Shel Morgan <shelbymorganbell@gmail.com>
2016-05-31 17:54:42 -0700

    updated jquery events

--- js/app.js
+++ js/app.js
@@ -2,5 +2,22 @@ $(document).ready(function() {
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
+  })
+  .mouseleave(function() {
+    $('.ryu-ready').hide();
+    $('.ryu-still').show();
+  })
+  .mousedown(function() {
+    console.log('mousedown');
+    //play hadouken sound
+    $('.ryu-ready').hide();
+    $('.ryu-throwing').show();
+    $('.hadouken').show();
+    // animate hadouken to the right of the screen
+  })
+  .mouseup(function() {
+    console.log('mouseup')
+    $('.ryu-throwing').hide();
+    $('.ryu-ready').show();
   });
 });
